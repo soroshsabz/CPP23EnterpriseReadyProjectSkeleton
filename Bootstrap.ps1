aad9772dfbe42002842bfc7117cdf3eeaf4f9698
soroshsabz <soorosh_abi@hotmail.com>
2026-01-29 11:37:42 +0330

    Add some minor fixes

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -1,4 +1,15 @@
 # ITNOA
 
-$env:VCPKG_ROOT="D:\vcpkg"
+$vcpkgFolder = "D:\vcpkg"
+if (Test-Patch -Path $vcpkgFolder) {
+    $env:VCPKG_ROOT = $vcpkgFolder
+} else {
+    $vcpkgFolder = "D:\Source\Repos\vcpkg"
+    if (Test-Path -Path $vcpkgFolder) {
+        $env:VCPKG_ROOT = $vcpkgFolder
+    } else {
+        Write-Error "vcpkg path does not found"
+    }
+}
+
 $env:PATH="$env:VCPKG_ROOT;$env:PATH"
