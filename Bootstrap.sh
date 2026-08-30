47fb263eed8b1acdd95a934e7cb07e639f3ed17a
soroshsabz <soorosh_abi@hotmail.com>
2026-08-30 16:15:47 +0330

    Make compatible Bootstrap.sh when system just have root user

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -37,22 +37,26 @@ function main() {
 }
 
 function prepare_cmake() {
-    local cmake_version=$(cmake --version | head -n 1 | cut -d ' ' -f 3)
+    echo "Preparing CMake..."
+    local cmake_version
+    cmake_version=$(cmake --version 2>/dev/null | head -n 1 | cut -d ' ' -f 3 || echo "0")
     readonly cmake_version
     echo "CMake version is: ${cmake_version}"
 
-    if [[ ${cmake_version} < 3.31  ]] ; then
+    if dpkg --compare-versions "${cmake_version}" "lt" "3.31"; then
         echo "cmake version is lower than required"
         install_cmake
     fi
 }
 
 function prepare_gcc() {
-    local gcc_version=$(gcc --version | head -n 1 | cut -d ' ' -f 4)
+    echo "Preparing GCC..."
+    local gcc_version
+    gcc_version=$(gcc --version 2>/dev/null | head -n 1 | cut -d ' ' -f 4 || echo "0")
     readonly gcc_version
     echo "GCC version is: ${gcc_version}"
 
-    if [[ ${gcc_version} < 16.0 ]] ; then
+    if dpkg --compare-versions "${gcc_version}" "lt" "16.0"; then
         echo "gcc version is lower than required"
         install_gcc
     fi
@@ -60,28 +64,34 @@ function prepare_gcc() {
 }
 
 function prepare_coverage_tool() {
-    local gcovr_version=$(gcovr --version | head -n 1 | cut -d ' ' -f 2)
+    echo "Preparing GCovr..."
+    local gcovr_version
+    gcovr_version=$(gcovr --version 2>/dev/null | head -n 1 | cut -d ' ' -f 2 || echo "0")
     readonly gcovr_version
     echo "GCovr version is: ${gcovr_version}"
 
-    if [[ ${gcovr_version} < 7.0 ]] ; then
+    if dpkg --compare-versions "${gcovr_version}" "lt" "7.0"; then
         echo "gcovr version is lower than required"
         install_coverage_tool
     fi
 }
 
 function prepare_vcpkg() {
-    local vcpkg_version=$(vcpkg --version | head -n 1 | cut -d ' ' -f 6)
+    echo "Preparing VCPKG..."
+    local vcpkg_version
+    vcpkg_version=$(vcpkg --version 2>/dev/null | head -n 1 | cut -d ' ' -f 6 || echo "0")
+    echo $?
     readonly vcpkg_version
     echo "VCPKG version is: ${vcpkg_version}"
 
-    if [[ ${vcpkg_version} < 2026 ]] ; then
+    if dpkg --compare-versions "${vcpkg_version}" "lt" "2026"; then
         echo "vcpkg version is lower than required"
         install_vcpkg
     fi
 }
 
 function install_cmake() {
+    echo "Installing CMake..."
     # TODO: Check distro and version
     apt install --assume-yes ca-certificates gpg wget
     test -f /usr/share/doc/kitware-archive-keyring/copyright ||
@@ -93,6 +103,7 @@ function install_cmake() {
 }
 
 function install_gcc() {
+    echo "Installing GCC..."
     # TODO: Check distro and version
     apt install --assume-yes g++-16
     update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-16 100
@@ -100,28 +111,32 @@ function install_gcc() {
 }
 
 function install_coverage_tool() {
+    echo "Installing GCovr..."
     # TODO: Check distro and version
     apt install --assume-yes gcovr
 }
 
 function install_vcpkg() {
+    echo "Installing VCPKG..."
     # FIXME: this method does not work for normal users
     apt install --assume-yes git
-    local original_user=$(journalctl _COMM=sudo | grep -i 'Bootstrap.sh' | head -n 1 | cut -d ' ' -f 6)
-    readonly originial_user
-    if [[ ${USER} == "root" ]] ; then
+    local original_user
+    original_user=$(journalctl _COMM=sudo | grep -i 'Bootstrap.sh' | head -n 1 | cut -d ' ' -f 6 || echo "")
+    readonly original_user
+    if [[ -n "${original_user}" && "${USER}" == "root" ]] ; then
         echo "Original User: ${original_user}"
-        sudo -u ${original_user} bash <<'EOF'
+        sudo -u "${original_user}" bash <<'EOF'
             cd ~ && git clone https://github.com/microsoft/vcpkg.git
             ~/vcpkg/bootstrap-vcpkg.sh
 
             cd ~
             if ! grep -q -- "VCPKG_ROOT" ~/.bashrc; then
                 echo 'export VCPKG_ROOT=`pwd`/vcpkg' >> ~/.bashrc
-                echo 'export PATH=$VCPKG_ROOT:$PATH' >> ~/.bashrc
+                echo "export PATH=$VCPKG_ROOT:$PATH" >> ~/.bashrc
             fi
 EOF
-        export VCPKG_ROOT=$(getent passwd ${original_user} | cut -d: -f6)/vcpkg
+        export VCPKG_ROOT
+        VCPKG_ROOT=$(getent passwd "${original_user}" | cut -d: -f6)/vcpkg
         export PATH=$VCPKG_ROOT:$PATH
     else
         cd ~ && git clone https://github.com/microsoft/vcpkg.git
@@ -130,12 +145,13 @@ EOF
         cd ~
         if ! grep -q -- "VCPKG_ROOT" ~/.bashrc; then
             echo 'export VCPKG_ROOT=~/vcpkg' >> ~/.bashrc
-            echo 'export PATH=$VCPKG_ROOT:$PATH' >> ~/.bashrc
+            echo "export PATH=$VCPKG_ROOT:$PATH" >> ~/.bashrc
         fi
 
-        source ~/.bashrc
+        export VCPKG_ROOT=~/vcpkg
+        export PATH=$VCPKG_ROOT:$PATH
     fi
-    ln -s ${VCPKG_ROOT}/vcpkg /usr/local/bin/vcpkg
+    ln -s "${VCPKG_ROOT}"/vcpkg /usr/local/bin/vcpkg
 }
 
 function finish() {
