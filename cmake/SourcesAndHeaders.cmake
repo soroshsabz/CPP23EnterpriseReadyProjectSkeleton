c4b4a99231742cacd087eaa0b1cd7521fb80acf1
soroshsabz <soorosh_abi@hotmail.com>
2026-09-17 17:16:30 +0330

    Rename some directory to windows friendly

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,27 +1,27 @@
 # ITNOA
 
 set(LIBRARY_SOURCES
-    src/ModuleA/Component1.cpp
-    src/SubTraditionalLibA/LibA.cpp
+    Source/ModuleA/Component1.cpp
+    Source/SubTraditionalLibA/LibA.cpp
 )
 
 set(LIBRARY_MODULES
-    src/ModuleA/Component1.ixx
-    src/ModuleA/ModuleA.ixx
+    Source/ModuleA/Component1.ixx
+    Source/ModuleA/ModuleA.ixx
 )
 
 set(APPLICATION_SOURCES
-		src/main.cpp
+	Source/main.cpp
 )
 
 set(LIBRARY_HEADERS
-    src/SubTraditionalLibA/LibA.h
+    Source/SubTraditionalLibA/LibA.h
 )
 
 set(LIBRARY_PUBLIC_HEADERS
-    include/PublicLibA.h
+    Include/PublicLibA.h
 )
 
 set(TEST_SOURCES
-  src/tmp_test.cpp
+  Tests/Component1Tests.cpp
 )
